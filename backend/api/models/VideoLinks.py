39aa0fb2636fa1ae9a7162a9dca58e341adbdf3e
Leundai <batmanlup2@gmail.com>
2020-10-01 17:50:39 -0500

    VideoLinks Model Created

---
+++ backend/api/models/VideoLinks.py
@@ -0,0 +1,13 @@
+from api.core import Mixin
+from .base import db
+from flask_mongoengine import Document
+from mongoengine import *
+
+class VideoLinks(Document, Mixin):
+    """Video Links Collection"""
+
+    Link = StringField(required=True)
+    Tag = StringField(required=True)
+
+    def __repr__(self):
+        return f"<VideoLink {self.Link}>"

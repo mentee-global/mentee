2d2139ee7c387512be2dacd3af903f2780eee542
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2020-10-06 16:34:25 -0500

    react routing

---
+++ frontend/src/pages/Appointments.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Appointments() {
+  return (
+    <div>
+      <header>
+        <p>
+          This is the appointments page
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Appointments;

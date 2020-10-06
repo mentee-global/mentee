578c8078b2230c361211f254f97e0a0e83c7a667
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2020-10-06 15:05:04 -0500

    remove lengths

--- backend/api/models/Education.py
+++ backend/api/models/Education.py
@@ -6,9 +6,9 @@ from mongoengine import *
 class Education(EmbeddedDocument, Mixin):
     """Education embedded within Mentor."""
 
-    education_level = StringField(max_length=200, required=True)
-    majors = ListField(StringField(), min_length=1, required=True)
-    school = StringField(max_length=200, required=True)
+    education_level = StringField(required=True)
+    majors = ListField(StringField(), required=True)
+    school = StringField(required=True)
     graduation_year = IntField(required=True)
 
     def __init__(self, education_level, majors, school, graduation_year):

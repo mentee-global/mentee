d11765e73d2b516932c9cb4100eaf3b579e974d1
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2020-10-08 18:01:05 -0500

    sidebar and header

--- frontend/src/components/pages/Appointments.js
+++ frontend/src/components/pages/Appointments.js
@@ -1,13 +1,67 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { Layout, Menu } from "antd";
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  CalendarOutlined,
+  HomeOutlined,
+} from "@ant-design/icons";
+
+import "antd/dist/antd.css";
+import "../css/Appointments.scss";
+
+import mentee_logo from "../../resources/mentee.png";
+
+const { Header, Sider, Content } = Layout;
 
 function Appointments() {
   return (
     <div>
-      <header>
-        <p>This is the appointments page</p>
-        <Link to="/">Back to Home</Link>
-      </header>
+      <Layout>
+        <Header className="appointments-header">
+          <img src={mentee_logo} alt="Mentee" className="mentee-logo"/>
+        </Header>
+        <Layout>
+          <Sider theme="light">
+            <Menu theme="light" mode="inline">
+              <Menu.Item
+                key="1"
+                className="appointments-menu-item"
+                icon={<HomeOutlined />}
+              >
+                <NavLink to="/" activeStyle={{ color: "black" }}>
+                  Home
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item
+                key="2"
+                className="appointments-menu-item"
+                icon={<CalendarOutlined />}
+              >
+                Appointments
+              </Menu.Item>
+              <Menu.Item
+                key="3"
+                className="appointments-menu-item"
+                icon={<VideoCameraOutlined />}
+              >
+                Your Videos
+              </Menu.Item>
+              <Menu.Item
+                key="4"
+                className="appointments-menu-item"
+                icon={<UserOutlined />}
+              >
+                Profile
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Content className="appointments-content">
+            Appointment page component goes here
+          </Content>
+        </Layout>
+      </Layout>
     </div>
   );
 }

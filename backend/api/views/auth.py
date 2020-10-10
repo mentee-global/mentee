65433fd4bc579db396e620ee7616b43b24ae3e7b
chyku <kelleyc2@illinois.edu>
2020-10-09 22:09:12 -0500

    Testing register endpoint

--- backend/api/views/auth.py
+++ backend/api/views/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from api.models import db, Person, Email
 from api.core import create_response, serialize_list, logger
+from api.utils.constants import AUTH_URL
 import requests
 
 auth = Blueprint("auth", __name__)  # initialize blueprint
@@ -8,9 +9,26 @@ auth = Blueprint("auth", __name__)  # initialize blueprint
 
 @auth.route("/verifyEmail", methods=["POST"])
 def verify_email():
-    headers = {"Content-Type": "application/json", "token": request.headers.token}
-    data = {"pin": request.body.pin}
-    results = requests.post(
-        "http://localhost:8000/verifyEmail", headers=headers, data=data
-    )
+    data = request.json
+    headers = {"Content-Type": "application/json", "token": request.headers["token"]}
+    data = {"pin": data.pin}
+    results = requests.post(AUTH_URL + "/verifyEmail", headers=headers, data=data)
     return create_response(data={results.json()})
+
+
+@auth.route("/register", methods=["POST"])
+def register():
+    data = request.json
+    # form validation here
+
+    headers = {"Content-Type": "application/json"}
+    results = requests.post(AUTH_URL + "/register/", data=data, headers=headers)
+    resp = results.json()
+    print(resp)
+    if not resp.get("token", None):
+        return create_response(message=resp["message"], status=400)
+
+    create_response(
+        message=resp.message,
+        data={"token": resp.token, "userID": resp.uid, "permission": resp.permission},
+    )

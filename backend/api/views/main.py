d3ae01f2448aeaffab85718edf08726f370d05f0
Daniel <dm32@illinois.edu>
2020-10-12 05:33:07 -0500

    reformat with black

--- backend/api/views/main.py
+++ backend/api/views/main.py
@@ -14,11 +14,13 @@ def index():
     logger.info("Hello World!")
     return "Hello World!"
 
+
 # function that is called when you visit /persons
 @main.route("/persons", methods=["GET"])
 def get_persons():
     persons = Person.objects()
     return create_response(data={"persons": persons})
+
 
 # POST request for /persons
 @main.route("/persons", methods=["POST"])
@@ -47,58 +49,66 @@ def create_person():
         message=f"Successfully created person {new_person.name} with id: {new_person.id}"
     )
 
+
 # PUT requests for /mentor
 @main.route("/mentor/<id>", methods=["PUT"])
 def edit_mentor(id):
     data = request.get_json()
-    
+
     logger.info("Data received: %s", data)
 
-    #Try to retrieve Mentor profile from database
-    try: 
+    # Try to retrieve Mentor profile from database
+    try:
         mentor = MentorProfile.objects.get(id=id)
     except:
         msg = "No mentor with that id"
         logger.info(msg)
         return create_response(status=422, message=msg)
 
+    # Check if data exists
     if data == None:
         msg = "No data provided to update Mentor Profile"
         logger.info(msg)
         return create_response(status=400, message=msg)
 
-
-    #Check for required fields
+    # Check for required fields
     mentor.name = data.get("name", mentor.name)
-    mentor.professional_title = data.get("professional_title", mentor.professional_title) 
+    mentor.professional_title = data.get(
+        "professional_title", mentor.professional_title
+    )
     mentor.specializations = data.get("specializations", mentor.specializations)
     mentor.languages = data.get("languages", mentor.languages)
-    mentor.offers_group_appointments = data.get("offers_group_appointments", mentor.offers_group_appointments) 
+    mentor.offers_group_appointments = data.get(
+        "offers_group_appointments", mentor.offers_group_appointments
+    )
     mentor.offers_in_person = data.get("offers_in_person", mentor.offers_in_person)
     mentor.biography = data.get("biography", mentor.biography)
     mentor.linkedin = data.get("linkedin", mentor.linkedin)
     mentor.website = data.get("website", mentor.website)
     mentor.picture = data.get("picture", mentor.picture)
-    
-    #Create education object
+
+    # Create education object
     if "education" in data:
         education_data = data.get("education")
-        new_education = Education(education_level=education_data.get("education_level"), 
-                                    majors = education_data.get("majors"),
-                                    school = education_data.get("school"), 
-                                    graduation_year = education_data.get("graduation_year"))
+        new_education = Education(
+            education_level=education_data.get("education_level"),
+            majors=education_data.get("majors"),
+            school=education_data.get("school"),
+            graduation_year=education_data.get("graduation_year"),
+        )
         mentor.education = new_education
 
-    #TODO: Determine if we keep videos in mentor profile    
-    #Create video object
+    # TODO: Determine if we keep videos in mentor profile
+    # Create video object
     if "video" in data:
         video_data = data.get("video")
-        new_video = Video(title=video_data.get("title"), url=video_data.get("url"), tag=video_data.get("tag"))
+        new_video = Video(
+            title=video_data.get("title"),
+            url=video_data.get("url"),
+            tag=video_data.get("tag"),
+        )
         mentor.video = new_video
 
     mentor.save()
 
-    return create_response(
-        status=200,
-        message=f"Success"
-    ) 
+    return create_response(status=200, message=f"Success")

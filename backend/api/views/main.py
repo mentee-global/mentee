c6bff9d21e5c0a38ac0bbdf0a116f8fa79fc2534
nroy20 <nroy20@illinois.edu>
2020-10-13 15:07:52 -0500

    Added GET request for single mentor based on ID

--- backend/api/views/main.py
+++ backend/api/views/main.py
@@ -21,6 +21,11 @@ def get_mentors():
     mentors = MentorProfile.objects()
     return create_response(data={"mentors": mentors})
 
+# GET request to get specific mentor based on id
+@main.route("/mentors/<string:mentor_id>", methods=["GET"])
+def get_mentor(mentor_id):
+    mentor = MentorProfile.objects().get(id=mentor_id)
+    return create_response(data={"mentor": mentor})
 
 # function that is called when you visit /persons
 @main.route("/persons", methods=["GET"])

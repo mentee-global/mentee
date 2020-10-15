b2728f06fa6485be705a42207d62fc689793a4d8
Leonardo Galindo <batmanlup2@gmail.com>
2020-10-15 17:59:36 -0500

    Removed index in video error message

--- backend/api/views/main.py
+++ backend/api/views/main.py
@@ -107,7 +107,7 @@ def create_mentor_profile():
             if not validate_video.validate():
                 msg = ", ".join(validate_video.errors.keys())
                 return create_response(
-                    status=422, message="Missing fields " + msg + " in video index "
+                    status=422, message="Missing fields " + msg
                 )
 
             new_video = Video(title=video["title"], url=video["url"], tag=video["tag"])

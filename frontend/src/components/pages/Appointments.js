e523ee25d415a7ada6cf108ffb658719b97ff633
Leundai <batmanlup2@gmail.com>
2020-10-19 17:30:43 -0500

    Added Appointment Component

--- frontend/src/components/pages/Appointments.js
+++ frontend/src/components/pages/Appointments.js
@@ -7,19 +7,19 @@ import { isCompositeComponent } from "react-dom/test-utils";
 const Tabs = Object.freeze({
   upcoming: {
     id: 1,
-    title: "All Upcoming"
+    title: "All Upcoming",
   },
   pending: {
     id: 2,
-    title: "All Pending"
+    title: "All Pending",
   },
   past: {
     id: 3,
-    title: "All Past"
+    title: "All Past",
   },
   appointments: {
     id: 4,
-    title: "TBD"
+    title: "TBD",
   },
 });
 
@@ -66,7 +66,6 @@ function Appointments() {
     );
   };
 
-
   const Appointment = (props) => {
     return (
       <div className="appointment-card">
@@ -75,63 +74,99 @@ function Appointments() {
           <div className="appointment-time">{props.time}</div>
           <div className="appointment-description">{props.description}</div>
         </div>
-        <Button className="appointment-more-details" icon={<MenuOutlined/>} type="text"></Button>
+        <Button
+          className="appointment-more-details"
+          icon={<MenuOutlined />}
+          type="text"
+        ></Button>
       </div>
-    )
-  }
+    );
+  };
 
   return (
     <div>
-        <Row gutter={4}>
-          <Col span={18}>
-            <div className="appointments-welcome-text">Welcome, Bernie</div>
-            <div className="appointments-tabs">
-              <Tab tab={Tabs.upcoming} text="Upcoming" />
-              <Tab tab={Tabs.pending} text="Pending" />
-              <Tab tab={Tabs.past} text="Past" />
-              <Tab tab={Tabs.appointments} text="Appointments" />
-            </div>
-            <b style={{
+      <Row gutter={4}>
+        <Col span={18}>
+          <div className="appointments-welcome-text">Welcome, Bernie</div>
+          <div className="appointments-tabs">
+            <Tab tab={Tabs.upcoming} text="Upcoming" />
+            <Tab tab={Tabs.pending} text="Pending" />
+            <Tab tab={Tabs.past} text="Past" />
+            <Tab tab={Tabs.appointments} text="Appointments" />
+          </div>
+          <b
+            style={{
               fontSize: "18px",
-            }}>{currentTab.title}</b>
+            }}
+          >
+            {currentTab.title}
+          </b>
 
-            <div className="appointments-background">
-              <div className="appointments-date">
-                <h1>10/6</h1>
-                <div style={{
+          <div className="appointments-background">
+            <div className="appointments-date">
+              <h1>10/6</h1>
+              <div
+                style={{
                   borderLeft: "solid",
                   borderColor: "#DBCA9E",
                   paddingLeft: "10px",
                   marginLeft: "10px",
                   borderWidth: "2px",
-                  width: "100%"
-                }}>
-                  <Appointment name="Leo" time="12pm - 2pm" description="the watersJust testing the watersJust testing the waters"/>
-                  <Appointment name="Leo" time="12pm - 2pm" description="the watersJust testing the watersJust testing the waters"/>
-                  <Appointment name="Leo" time="12pm - 2pm" description="the watersJust testing the watersJust testing the waters"/>
-                </div>
-              </div>
-              <div className="appointments-date">
-                <h1>10/6</h1>
-                <div style={{
-                  borderLeft: "solid",
-                  borderColor: "#DBCA9E",
-                  paddingLeft: "10px",
-                  marginLeft: "10px",
-                  borderWidth: "2px",
-                  width: "100%"
-                }}>
-                  <Appointment name="Leo" time="12pm - 2pm" description="the watersJust testing the watersJust testing the waters"/>
-                  <Appointment name="Leo" time="12pm - 2pm" description="the watersJust testing the watersJust testing the waters"/>
-                  <Appointment name="Leo" time="12pm - 2pm" description="the watersJust testing the watersJust testing the waters"/>
-                </div>
+                  width: "100%",
+                }}
+              >
+                <Appointment
+                  name="Leo"
+                  time="12pm - 2pm"
+                  description="the watersJust testing the watersJust testing the waters"
+                />
+                <Appointment
+                  name="Leo"
+                  time="12pm - 2pm"
+                  description="the watersJust testing the watersJust testing the waters"
+                />
+                <Appointment
+                  name="Leo"
+                  time="12pm - 2pm"
+                  description="the watersJust testing the watersJust testing the waters"
+                />
               </div>
             </div>
-          </Col>
-          <Col span={6} >
-            <Calendar></Calendar>
-          </Col>
-        </Row>
+            <div className="appointments-date">
+              <h1>10/6</h1>
+              <div
+                style={{
+                  borderLeft: "solid",
+                  borderColor: "#DBCA9E",
+                  paddingLeft: "10px",
+                  marginLeft: "10px",
+                  borderWidth: "2px",
+                  width: "100%",
+                }}
+              >
+                <Appointment
+                  name="Leo"
+                  time="12pm - 2pm"
+                  description="the watersJust testing the watersJust testing the waters"
+                />
+                <Appointment
+                  name="Leo"
+                  time="12pm - 2pm"
+                  description="the watersJust testing the watersJust testing the waters"
+                />
+                <Appointment
+                  name="Leo"
+                  time="12pm - 2pm"
+                  description="the watersJust testing the watersJust testing the waters"
+                />
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col span={6}>
+          <Calendar></Calendar>
+        </Col>
+      </Row>
     </div>
   );
 }

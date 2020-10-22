104187e8525154373f53e490f0c28be0523961cc
Daniel <dm32@illinois.edu>
2020-10-22 09:20:30 -0500

    formatting

--- backend/api/views/appointment.py
+++ backend/api/views/appointment.py
@@ -3,6 +3,7 @@ from api.models import db, Person, Email, Availability, AppointmentRequest
 from api.core import create_response, serialize_list, logger
 import requests
 from dateutil.parser import parse
+
 appointment = Blueprint("appointment", __name__)
 
 
@@ -27,15 +28,16 @@ def put_appointment(id):
 
     return create_response(status=200, message=f"Success")
 
+
 @appointment.route("/test", methods=["POST"])
 def addtestcollection():
     new_appointment = AppointmentRequest(
         mentor_id="5f7f92fd1ad5465da13d4635",
-        timeslot= Availability(
-            start_time = parse("Thu Oct 22 17:13:46 UTC 2020"),
-            end_time = parse("Thu Oct 22 18:13:46 UTC 2020")
+        timeslot=Availability(
+            start_time=parse("Thu Oct 22 17:13:46 UTC 2020"),
+            end_time=parse("Thu Oct 22 18:13:46 UTC 2020"),
         ),
-        age = "12",
+        age="12",
         accepted=False,
         name="Bob Ross",
         email="bobross@gmail.com",
@@ -47,12 +49,9 @@ def addtestcollection():
         mentorship_goals="Something",
         specialist_categories=["stuff"],
         message="Hello world",
-        attendee_count = 13,
-        organization="Hack4Impact"
+        attendee_count=13,
+        organization="Hack4Impact",
     )
 
     new_appointment.save()
-    return create_response(
-        status=200, message=f"Success"
-    )
-    
+    return create_response(status=200, message=f"Success")

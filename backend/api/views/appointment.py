74be92f49a37e26930603bf8997a2704e005fbea
Daniel <dm32@illinois.edu>
2020-10-22 09:18:59 -0500

    testing endpoint

--- backend/api/views/appointment.py
+++ backend/api/views/appointment.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from api.models import db, Person, Email, Availability, AppointmentRequest
 from api.core import create_response, serialize_list, logger
 import requests
-
+from dateutil.parser import parse
 appointment = Blueprint("appointment", __name__)
 
 
@@ -27,30 +27,32 @@ def put_appointment(id):
 
     return create_response(status=200, message=f"Success")
 
-@appointment.route("/testendpoint", methods=["POST"])
+@appointment.route("/test", methods=["POST"])
 def addtestcollection():
     new_appointment = AppointmentRequest(
+        mentor_id="5f7f92fd1ad5465da13d4635",
         timeslot= Availability(
             start_time = parse("Thu Oct 22 17:13:46 UTC 2020"),
             end_time = parse("Thu Oct 22 18:13:46 UTC 2020")
-        )
-        accepted=False
-        name="Bob Ross"
-        email="bobross@gmail.com"
-        phone_number="(111) 111-1111"
-        languages=["English", "Art"]
-        gender="Male"
-        ethnicity="Caucasian"
-        location="Somewhere"
-        mentorship_goals="Something"
-        specialist_categories=["stuff"]
-        message="Hello world"
-        attendee_count = 13
+        ),
+        age = "12",
+        accepted=False,
+        name="Bob Ross",
+        email="bobross@gmail.com",
+        phone_number="(111) 111-1111",
+        languages=["English", "Art"],
+        gender="Male",
+        ethnicity="Caucasian",
+        location="Somewhere",
+        mentorship_goals="Something",
+        specialist_categories=["stuff"],
+        message="Hello world",
+        attendee_count = 13,
         organization="Hack4Impact"
     )
 
-    newappointment.save()
-    return create_respons(
+    new_appointment.save()
+    return create_response(
         status=200, message=f"Success"
     )
     

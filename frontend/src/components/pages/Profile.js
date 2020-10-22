3d324f3a668042cf6157e49e4f29563c9a02f402
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2020-10-22 17:49:47 -0500

    DIVS FOR EVERYONEgit add *!

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -1,16 +1,64 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { UserOutlined } from "@ant-design/icons";
+import {
+  UserOutlined,
+  EnvironmentOutlined,
+  CommentOutlined,
+  LinkOutlined,
+  LinkedinOutlined,
+} from "@ant-design/icons";
 
 import "../css/Profile.scss";
 
 function Profile() {
   return (
     <div className="background-color-strip">
-      <div className="profile-content">
+      <div className="mentor-profile-content">
         <UserOutlined style={{ fontSize: "100px" }} />
-        <br />
-        This is the profile body
+        <div className="mentor-profile-content-flexbox">
+          <div className="mentor-profile-info">
+            <div className="mentor-profile-name">Mentor Name</div>
+            <div className="mentor-profile-heading">
+              Title <t className="yellow-dot">•</t> Meetings[0] | Meetings[1]
+            </div>
+            <div>
+              <span>
+                <EnvironmentOutlined className="mentor-profile-tag-first" />
+                Location
+              </span>
+              <span>
+                <CommentOutlined className="mentor-profile-tag" />
+                Language[0] • Language[1]
+              </span>
+              <span>
+                <LinkOutlined className="mentor-profile-tag" />
+                website.com
+              </span>
+              <span>
+                <LinkedinOutlined className="mentor-profile-tag" />
+                LinkedIn
+              </span>
+            </div>
+            <br />
+            <div className="mentor-profile-heading">
+              <b>About</b>
+            </div>
+            <div>About text</div>
+            <br />
+            <div className="mentor-profile-heading">
+              <b>Specializations</b>
+            </div>
+            <div>Tags tags tags</div>
+            <br />
+            <div className="mentor-profile-heading">
+              <b>Education</b>
+            </div>
+            <div>Education list</div>
+          </div>
+          <div className="mentor-profile-contact">
+            <div className="mentor-profile-contact-header">Contact Info</div>
+          </div>
+        </div>
       </div>
     </div>
   );

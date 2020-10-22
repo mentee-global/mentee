7cfc707f800db7bdd0e22637d0bb4ca44746b920
Daniel <dm32@illinois.edu>
2020-10-22 08:36:42 -0500

    documentation and formatting

--- backend/api/views/appointment.py
+++ backend/api/views/appointment.py
@@ -5,26 +5,22 @@ import requests
 
 appointment = Blueprint("appointment", __name__)
 
+
 @appointment.route("/appointment/<id>", methods=["PUT"])
 def put_appointment(id):
     data = request.get_json()
 
     logger.info("Data received: %s", data)
 
+    # Try to get appointment
     try:
         appointment = AppointmentRequest.get(id=id)
     except:
         msg = "No appointment with that id"
         logger.info(msg)
         return create_response(status=422, message=msg)
-    
-    if "timeslot" in data:
-        availability_data = data.get("availability")
-        appointment.timeslot = Availability(
-            start_time=availability_data.get("start_time"),
-            end_time=availability_data.get("end_data")
-        )
-    
+
+    # Update appointment acceptance status
     appointment.accepted = data.get("accepted", appointment.accepted)
 
     appointment.save()

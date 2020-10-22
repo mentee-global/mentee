e56c0931a657b8593787f43749a2d5d9f72595e6
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2020-10-22 16:39:12 -0500

    remove rounded border

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { UserOutlined } from "@ant-design/icons";
 
 import "../css/Profile.scss";
 
 function Profile() {
-  return <div className="background-color-strip">Profile</div>;
+  return (
+    <div>
+      <div className="background-color-strip" />
+      <div className="profile-content">
+        <UserOutlined style={{ fontSize: "100px" }} />
+      </div>
+    </div>
+  );
 }
 
 export default Profile;

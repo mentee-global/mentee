051858928b4df563cbf6eec846cacbb650aa6e3f
Lam <Lam7150@gmail.com>
2020-10-24 14:44:22 -0500

    commit

---
+++ frontend/src/components/MentorProfileModal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "antd";
+
+function MentorProfileModal() {
+    return (
+        <Button
+            type="default"
+            shape="round"
+            onClick={() => console.log('GOO')}
+        >Edit Profile</Button>
+    )
+}
+
+export default MentorProfileModal;
--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -1,8 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Button, Modal } from "antd";
+import MentorProfileModal from "../MentorProfileModal";
 
 function Profile() {
-  return <div>Profile</div>;
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <div>
+      <Button type="primary" onClick={() => setModalVisible(true)}>
+        Open Modal
+        </Button>
+      <Modal
+        visible={modalVisible}
+        bodyStyle={{ padding: 0 }}
+        footer={<Button
+          type="default"
+          shape="round"
+          style={{
+            borderRadius: 13,
+            marginRight: 15,
+            backgroundColor: "#E4BB4F"
+          }}
+          onClick={() => setModalVisible(false)}
+        >
+          Save</Button>}
+      >
+        <div style={{ height: 1000, width: "100%", backgroundColor: "yellow" }}>
+
+        </div>
+      </Modal>
+    </div>
+
+  );
 }
 
 export default Profile;

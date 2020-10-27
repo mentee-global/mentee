6c708f19d06dcf04813eb4b74caa4f67c7b7d677
nroy20 <nroy20@illinois.edu>
2020-10-27 15:46:56 -0500

    small updates

--- backend/api/views/appointment.py
+++ backend/api/views/appointment.py
@@ -7,8 +7,8 @@ from dateutil.parser import parse
 appointment = Blueprint("appointment", __name__)
 
 # GET request for appointments by mentor id
-@appointment.route("/appointment/<string:mentor_id>", methods=["GET"])
-def get_requests(mentor_id):
+@appointment.route("/appointment/mentor/<string:mentor_id>", methods=["GET"])
+def get_requests_by_mentor(mentor_id):
     requests = AppointmentRequest.objects(mentor_id=mentor_id)
     return create_response(data={"requests": requests})
 
@@ -19,14 +19,13 @@ def delete_request(appointment_id):
     try:
         request = AppointmentRequest.objects.get(id=appointment_id)
         request.delete()
-    except Exception as e:
-        print(e)
+    except:
         msg = "The request you attempted to delete was not found"
         logger.info(msg)
         return create_response(status=422, message=msg)
     return create_response(data={"deleted id": appointment_id})
 
-
+    
 @appointment.route("/appointment/accept/<id>", methods=["PUT"])
 def put_appointment(id):
     data = request.get_json()

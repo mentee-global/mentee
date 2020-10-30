2861717de06e9252b8a0d2ea2f2ae9fe6d43b1f0
chyku <kelleyc2@illinois.edu>
2020-10-29 21:57:10 -0500

    register working

--- backend/api/utils/constants.py
+++ backend/api/utils/constants.py
@@ -1,1 +1,2 @@
-AUTH_URL = "https://h4i-mentee-auth.chyku.vercel.app/"
+AUTH_URL = "https://h4i-mentee-auth.vercel.app"
+# AUTH_URL = "http://localhost:8000"
--- backend/api/views/auth.py
+++ backend/api/views/auth.py
@@ -10,25 +10,30 @@ auth = Blueprint("auth", __name__)  # initialize blueprint
 @auth.route("/verifyEmail", methods=["POST"])
 def verify_email():
     data = request.json
-    headers = {"Content-Type": "application/json", "token": request.headers["token"]}
-    data = {"pin": data.pin}
-    results = requests.post(AUTH_URL + "/verifyEmail", headers=headers, data=data)
+    headers = {
+        "Content-Type": "application/json",
+        "token": request.headers.get("token"),
+    }
+    results = requests.post(AUTH_URL + "/verifyEmail", headers=headers, json=data)
     return create_response(data={results.json()})
 
 
 @auth.route("/register", methods=["POST"])
 def register():
     data = request.json
-    # form validation here
-
+    print(data)
     headers = {"Content-Type": "application/json"}
-    results = requests.post(AUTH_URL + "/register/", data=data, headers=headers)
+    results = requests.post(AUTH_URL + "/register", headers=headers, json=data)
     resp = results.json()
     print(resp)
-    if not resp.get("token", None):
+    if not resp.get("token"):
         return create_response(message=resp["message"], status=400)
 
-    create_response(
-        message=resp.message,
-        data={"token": resp.token, "userID": resp.uid, "permission": resp.permission},
+    return create_response(
+        message=resp["message"],
+        data={
+            "token": resp["token"],
+            "userID": resp["uid"],
+            "permission": resp["permission"],
+        },
     )

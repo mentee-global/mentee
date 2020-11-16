e15439530d44e1210e08b9c11ca8737395fe7bd6
Leundai <lgalin6@illinois.edu>
2020-11-16 17:43:19 -0600

    thank you for your service laptop, you ded to me know

--- frontend/src/components/pages/Videos.js
+++ frontend/src/components/pages/Videos.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Row, Col, Button, Form, Input } from "antd";
+import { Row, Col, Button, Form, Input, Select } from "antd";
 import "../css/Videos.scss";
+const { Option } = Select;
 
 function Videos() {
   return (
@@ -11,34 +12,49 @@ function Videos() {
           Manage Uploads
         </Button>
       </div>
-      <Row>
-        <Col span={16}>Hello</Col>
-        <div className="video-submit-card">
-          <h1 className="video-submit-title">Add Videos</h1>
-          <Form
-            name="video-submit"
-            initialValues={{
-              remember: true,
-            }}
-          >
-            <Form.Item
-              name="url"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input a video link",
-                },
-              ]}
-            >
-              <Input placeholder="Paste Link" />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
+      <div className="filter-card">
+        <h1>Your Uploads</h1>
+        <div className="filters">
+          <Input.Search></Input.Search>
+          <Select>
+            <Option>First</Option>
+            <Option>Second</Option>
+            <Option>Third</Option>
+          </Select>
         </div>
+      </div>
+      <Row>
+        <Col span={16}>
+          <div className="videos-container">hello</div>
+        </Col>
+        <Col span={8}>
+          <div className="video-submit-card">
+            <h1 className="video-submit-title">Add Videos</h1>
+            <Form
+              name="video-submit"
+              initialValues={{
+                remember: true,
+              }}
+            >
+              <Form.Item
+                name="url"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input a video link",
+                  },
+                ]}
+              >
+                <Input placeholder="Paste Link" />
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Col>
       </Row>
     </div>
   );

b5c514cf7a05257eb745a1dd27887a02d06af0c4
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2020-11-18 17:34:34 -0600

    move styles

---
+++ frontend/src/components/MenteeButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "antd";
+import "./css/MenteeButton.scss";
+
+function MenteeButton(props) {
+  return (
+    <div></div>
+  ); 
+}
+
+export default MenteeButton;

3a57cf2e1d3c636766c76863b0c07d9f4e3c29e2
Lam <Lam7150@gmail.com>
2020-11-25 21:47:41 -0500

    finished styling

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -4,16 +4,14 @@ import { Input } from "antd";
 import MenteeButton from "../MenteeButton";
 
 import "../css/Home.scss";
-import "../css/SignIn.scss"
+import "../css/SignIn.scss";
 import Honeycomb from "../../resources/honeycomb.png";
 
 function Home() {
   const [authenticated, setAuth] = useState(true);
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-  const [inputClicked, setInputClicked] = useState(
-    new Array(2).fill(false)
-  ); // Sets one boolean for email and password
+  const [inputClicked, setInputClicked] = useState(new Array(2).fill(false)); // Sets one boolean for email and password
 
   function handleClick(index) {
     // Sets only the clicked input box to true to change color, else false
@@ -32,61 +30,78 @@ function Home() {
 
   const HomePage = () => {
     if (authenticated) {
-      return (<div className="signin-content">
-        <div className="signin-container">
-          <h1 className="signin-text">Sign In</h1>
-          <div className={`signin-input-container${(inputClicked[0] ? "__clicked" : "")}`}>
-            <Input
-              className="signin-input"
-              onClick={() => handleClick(0)}
-              onChange={e => handleEmailChange(e)}
-              bordered={false}
-              placeholder="Email"
-            />
+      return (
+        <div className="signin-content">
+          <div className="signin-container">
+            <h1 className="signin-text">Sign In</h1>
+            <div
+              className={`signin-input-container${
+                inputClicked[0] ? "__clicked" : ""
+              }`}
+            >
+              <Input
+                className="signin-input"
+                onClick={() => handleClick(0)}
+                onChange={(e) => handleEmailChange(e)}
+                bordered={false}
+                placeholder="Email"
+              />
+            </div>
+            <div
+              className={`signin-input-container${
+                inputClicked[1] ? "__clicked" : ""
+              }`}
+            >
+              <Input
+                className="signin-input"
+                onClick={() => handleClick(1)}
+                onChange={(e) => handlePasswordChange(e)}
+                bordered={false}
+                placeholder="Password"
+              />
+            </div>
+            <div className="signin-button">
+              <MenteeButton
+                content={<b>Login</b>}
+                width={150}
+                onClick={() => {}}
+              />
+            </div>
+            <div className="signin-register-container">
+              <div>Don't have an account?</div>
+              <button
+                className="signin-register-button"
+                onClick={() => console.log("clicked")}
+              >
+                Register
+              </button>
+            </div>
           </div>
-          <div className={`signin-input-container${(inputClicked[1] ? "__clicked" : "")}`}>
-            <Input
-              className="signin-input"
-              onClick={() => handleClick(1)}
-              onChange={e => handlePasswordChange(e)}
-              bordered={false}
-              placeholder="Password"
-            />
-          </div>
-          <div className="signin-button">
-            <MenteeButton content={<b>Login</b>} width={150} onClick={() => { }} />
-          </div>
-          <div className="signin-register-container">
-            <div>Don't have an account?</div>
-            <div className="signin-register-text">Register</div>
-          </div>
+          <img className="home-honeycomb" src={Honeycomb} alt="" />
         </div>
-        <img className="home-honeycomb" src={Honeycomb} alt="" />
-      </div>)
+      );
     } else {
-      return (<div className="home-content">
-        <div className="home-text-container">
-          <h1 className="home-header">Welcome to Mentee</h1>
-          <p className="home-text">
-            Find a mentors from a diverse pool of backgrounds with experience in
-            18+ specializations, 15 different languages at locations all across
-            the country.
-        </p>
-          <br />
-          <NavLink to="/gallery">
-            <MenteeButton theme="dark" content={<b>Find a Mentor</b>} />
-          </NavLink>
+      return (
+        <div className="home-content">
+          <div className="home-text-container">
+            <h1 className="home-header">Welcome to Mentee</h1>
+            <p className="home-text">
+              Find a mentors from a diverse pool of backgrounds with experience
+              in 18+ specializations, 15 different languages at locations all
+              across the country.
+            </p>
+            <br />
+            <NavLink to="/gallery">
+              <MenteeButton theme="dark" content={<b>Find a Mentor</b>} />
+            </NavLink>
+          </div>
+          <img className="home-honeycomb" src={Honeycomb} alt="" />
         </div>
-        <img className="home-honeycomb" src={Honeycomb} alt="" />
-      </div>)
+      );
     }
-  }
+  };
 
-  return (
-    <div className="home-background">
-      {HomePage()}
-    </div>
-  );
+  return <div className="home-background">{HomePage()}</div>;
 }
 
 export default Home;

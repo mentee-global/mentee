74c2de752766a449f5dd083f427f9e9f0e304862
Leundai <batmanlup2@gmail.com>
2020-12-15 15:59:09 -0600

    added: video submit styling

--- frontend/src/components/VideoSubmit.js
+++ frontend/src/components/VideoSubmit.js
@@ -6,55 +6,63 @@ import "../components/css/Videos.scss";
 
 const VideoSubmit = (props) => {
   return (
-    <div className="video-submit-card">
-      <h1 className="video-submit-title">Add Video</h1>
-      <Form
-        form={props.form}
-        name="video-submit"
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={props.handleSubmitVideo}
-      >
-        <Form.Item
-          name="title"
-          rules={[
-            {
-              required: true,
-              message: "Please input a video title",
-            },
-          ]}
+    <div
+      style={{
+        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+        marginRight: "36px",
+        borderRadius: "8px",
+      }}
+    >
+      <div className="video-submit-title">Add Video</div>
+      <div className="video-submit-card">
+        <Form
+          form={props.form}
+          name="video-submit"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={props.handleSubmitVideo}
         >
-          <Input placeholder="Video Title" />
-        </Form.Item>
-        <Form.Item
-          name="url"
-          rules={[
-            {
-              required: true,
-              message: "Please input a video link",
-            },
-          ]}
-        >
-          <Input placeholder="Video Link" />
-        </Form.Item>
-        <Form.Item
-          name="tag"
-          rules={[
-            {
-              required: true,
-              message: "Please select a tag",
-            },
-          ]}
-        >
-          <Select>{formatDropdownItems(SPECIALIZATIONS)}</Select>
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item
+            name="title"
+            rules={[
+              {
+                required: true,
+                message: "Please input a video title",
+              },
+            ]}
+          >
+            <Input placeholder="Video Title" />
+          </Form.Item>
+          <Form.Item
+            name="url"
+            rules={[
+              {
+                required: true,
+                message: "Please input a video link",
+              },
+            ]}
+          >
+            <Input placeholder="Video Link" />
+          </Form.Item>
+          <Form.Item
+            name="tag"
+            rules={[
+              {
+                required: true,
+                message: "Please select a tag",
+              },
+            ]}
+          >
+            <Select>{formatDropdownItems(SPECIALIZATIONS)}</Select>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };

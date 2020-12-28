64bb83d8a44ce6f9398c7c77d27c394da62709f1
nroy20 <nroy20@illinois.edu>
2020-12-27 22:35:04 -0600

    formatting

--- backend/api/views/main.py
+++ backend/api/views/main.py
@@ -18,6 +18,7 @@ def get_mentors():
     mentors = MentorProfile.objects()
     return create_response(data={"mentors": mentors})
 
+
 # GET request for specific mentor based on id
 @main.route("/mentor/<string:mentor_id>", methods=["GET"])
 def get_mentor(mentor_id):

21e9a419482ee832135182c87040d857310a2aa8
Lam <Lam7150@gmail.com>
2021-01-07 16:22:16 -0500

    converting to basic inputs -- draft

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { UserOutlined, MailOutlined, PhoneOutlined } from "@ant-design/icons";
-import { Form, Input, Avatar, Switch, Button } from "antd";
+import { Input, Avatar, Switch, Button } from "antd";
 import { getMentorID } from "utils/auth.service";
 import ProfileContent from "../ProfileContent";
+import MenteeButton from "../MenteeButton";
 
-import "../css/MenteeButton.scss";
 import "../css/Profile.scss";
 import { fetchMentorByID } from "utils/api";
 
@@ -12,13 +12,13 @@ function Profile() {
   const [mentor, setMentor] = useState({});
   const [onEdit, setEditing] = useState(false);
   const [editedMentor, setEditedMentor] = useState(false);
-  const [form] = Form.useForm();
 
   useEffect(() => {
     const mentorID = getMentorID();
     async function getMentor() {
       const mentorData = await fetchMentorByID(mentorID);
       if (mentorData) {
+        console.log(mentorData);
         setMentor(mentorData);
       }
     }
@@ -30,106 +30,66 @@ function Profile() {
   };
 
   function renderContactInfo() {
-    return (<div>
-      {mentor.email && (
-        <div>
-          <MailOutlined className="mentor-profile-contact-icon" />
-          {mentor.email}
-          <br />
-        </div>
-      )}
-      {mentor.phone_number && (
-        <div>
-          <PhoneOutlined className="mentor-profile-contact-icon" />
-          {mentor.phone_number}
-          <br />
-        </div>
-      )}
-      <br />
-      <a href="#" onClick={() => setEditing(true)} className="mentor-profile-contact-edit">
-        Edit
-      </a>
-    </div>);
+    return (
+      <div>
+        {mentor.email && (
+          <div>
+            <MailOutlined className="mentor-profile-contact-icon" />
+            {mentor.email}
+            <br />
+          </div>
+        )}
+        {mentor.phone_number && (
+          <div>
+            <PhoneOutlined className="mentor-profile-contact-icon" />
+            {mentor.phone_number}
+            <br />
+          </div>
+        )}
+        <br />
+        <a
+          href="#"
+          onClick={() => setEditing(true)}
+          className="mentor-profile-contact-edit"
+        >
+          Edit
+        </a>
+      </div>
+    );
   }
-  const validateMessages = {
-    required: '${label} is required!',
-    types: {
-      email: '${label} is not a valid email!',
-      number: '${label} is not a valid number!',
-    },
-  };
-
-  const onFinish = (values) => {
-    setEditing(false);
-    console.log(values);
-  };
 
   function renderEditInfo() {
-    // Setting initial values with custom components
-    form.setFieldsValue({
-      email: mentor.email
-    });
-
-    return (<Form form={form}
-      name="nest-messages" onFinish={onFinish} validateMessages={validateMessages} initialValues={{ email: "oogabooga" }}>
-      <Form.Item
-        name="email"
-        rules={[
-          {
-            required: true,
-            type: 'email',
-          },
-        ]}
-      >
+    return (
+      <div>
         <div className="mentor-profile-input">
           <MailOutlined className="mentor-profile-contact-icon" />
-          <Input />
+          <Input defaultValue={mentor.email} />
         </div>
-      </Form.Item>
-      <Form.Item
-        name="phone_number"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
         <div className="mentor-profile-input">
           <PhoneOutlined className="mentor-profile-contact-icon" />
           <Input defaultValue={mentor.phone_number} />
         </div>
-      </Form.Item>
-      <Form.Item
-        name="email_notifications"
-      >
-        <div className="modal-mentee-availability-switch">
-          <div className="modal-mentee-availability-switch-text">
-            Email notifications
-                    </div>
-          <Switch
-            size="small"
-          />
-        </div>
-      </Form.Item>
-      <Form.Item name="text_notifications">
-        <div className="modal-mentee-availability-switch">
-          <div className="modal-mentee-availability-switch-text">
-            Text notifications
+        <div className="mentor-profile-editing-footer">
+          <div className="mentor-profile-notifications-container">
+            <div className="modal-mentee-availability-switch">
+              <div className="modal-mentee-availability-switch-text">
+                Email notifications
+              </div>
+              <Switch size="small" />
+            </div>
+            <div className="modal-mentee-availability-switch">
+              <div className="modal-mentee-availability-switch-text">
+                Text notifications
+              </div>
+              <Switch size="small" />
+            </div>
           </div>
-          <Switch
-            size="small"
-          />
+          <div className="mentor-profile-save-container">
+            <MenteeButton onClick={() => setEditing(false)} content="Save" />
+          </div>
         </div>
-      </Form.Item>
-      <Form.Item>
-        <Button
-          className="regular-button"
-          htmlType="submit"
-        >
-          Save
-    </Button>
-      </Form.Item>
-    </Form>);
+      </div>
+    );
   }
 
   return (

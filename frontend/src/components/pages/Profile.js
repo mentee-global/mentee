fa4f422baf77691669aa529a001607d4fbf31b3d
Lam <Lam7150@gmail.com>
2021-01-06 21:39:29 -0500

    Initial form with antdesign forms

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { NavLink } from "react-router-dom";
 import { UserOutlined, MailOutlined, PhoneOutlined } from "@ant-design/icons";
-import { Avatar } from "antd";
+import { Form, Input, Avatar, Switch, Button } from "antd";
 import { getMentorID } from "utils/auth.service";
 import ProfileContent from "../ProfileContent";
 
+import "../css/MenteeButton.scss";
 import "../css/Profile.scss";
 import { fetchMentorByID } from "utils/api";
 
 function Profile() {
   const [mentor, setMentor] = useState({});
+  const [onEdit, setEditing] = useState(false);
   const [editedMentor, setEditedMentor] = useState(false);
+  const [form] = Form.useForm();
 
   useEffect(() => {
     const mentorID = getMentorID();
@@ -26,6 +28,109 @@ function Profile() {
   const handleSaveEdits = () => {
     setEditedMentor(!editedMentor);
   };
+
+  function renderContactInfo() {
+    return (<div>
+      {mentor.email && (
+        <div>
+          <MailOutlined className="mentor-profile-contact-icon" />
+          {mentor.email}
+          <br />
+        </div>
+      )}
+      {mentor.phone_number && (
+        <div>
+          <PhoneOutlined className="mentor-profile-contact-icon" />
+          {mentor.phone_number}
+          <br />
+        </div>
+      )}
+      <br />
+      <a href="#" onClick={() => setEditing(true)} className="mentor-profile-contact-edit">
+        Edit
+      </a>
+    </div>);
+  }
+  const validateMessages = {
+    required: '${label} is required!',
+    types: {
+      email: '${label} is not a valid email!',
+      number: '${label} is not a valid number!',
+    },
+  };
+
+  const onFinish = (values) => {
+    setEditing(false);
+    console.log(values);
+  };
+
+  function renderEditInfo() {
+    // Setting initial values with custom components
+    form.setFieldsValue({
+      email: mentor.email
+    });
+
+    return (<Form form={form}
+      name="nest-messages" onFinish={onFinish} validateMessages={validateMessages} initialValues={{ email: "oogabooga" }}>
+      <Form.Item
+        name="email"
+        rules={[
+          {
+            required: true,
+            type: 'email',
+          },
+        ]}
+      >
+        <div className="mentor-profile-input">
+          <MailOutlined className="mentor-profile-contact-icon" />
+          <Input />
+        </div>
+      </Form.Item>
+      <Form.Item
+        name="phone_number"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <div className="mentor-profile-input">
+          <PhoneOutlined className="mentor-profile-contact-icon" />
+          <Input defaultValue={mentor.phone_number} />
+        </div>
+      </Form.Item>
+      <Form.Item
+        name="email_notifications"
+      >
+        <div className="modal-mentee-availability-switch">
+          <div className="modal-mentee-availability-switch-text">
+            Email notifications
+                    </div>
+          <Switch
+            size="small"
+          />
+        </div>
+      </Form.Item>
+      <Form.Item name="text_notifications">
+        <div className="modal-mentee-availability-switch">
+          <div className="modal-mentee-availability-switch-text">
+            Text notifications
+          </div>
+          <Switch
+            size="small"
+          />
+        </div>
+      </Form.Item>
+      <Form.Item>
+        <Button
+          className="regular-button"
+          htmlType="submit"
+        >
+          Save
+    </Button>
+      </Form.Item>
+    </Form>);
+  }
 
   return (
     <div className="background-color-strip">
@@ -47,24 +152,7 @@ function Profile() {
             <legend className="mentor-profile-contact-header">
               Contact Info
             </legend>
-            {mentor.email && (
-              <div>
-                <MailOutlined className="mentor-profile-contact-icon" />
-                {mentor.email}
-                <br />
-              </div>
-            )}
-            {mentor.phone_number && (
-              <div>
-                <PhoneOutlined className="mentor-profile-contact-icon" />
-                {mentor.phone_number}
-                <br />
-              </div>
-            )}
-            <br />
-            <NavLink to="/" className="mentor-profile-contact-edit">
-              Edit
-            </NavLink>
+            {onEdit ? renderEditInfo() : renderContactInfo()}
           </fieldset>
         </div>
       </div>

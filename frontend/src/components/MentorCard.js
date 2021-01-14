655c268cf2943725c95e53402e32cdf10085c407
chyku <kelleyc2@illinois.edu>
2021-01-14 14:37:15 -0500

    fixing linking

--- frontend/src/components/MentorCard.js
+++ frontend/src/components/MentorCard.js
@@ -74,7 +74,11 @@ function MentorCard(props) {
         {props.website && (
           <h4>
             <LinkOutlined style={styles.icon} />
-            <a className="gallery-links" href={props.website}>
+            <a
+              className="gallery-links"
+              href={"//" + props.website}
+              target="_blank"
+            >
               {props.website}
             </a>
           </h4>
@@ -82,7 +86,11 @@ function MentorCard(props) {
         {props.linkedin && (
           <h4>
             <LinkedinOutlined style={styles.icon} />
-            <a className="gallery-links" href={props.linkedin}>
+            <a
+              className="gallery-links"
+              href={"//" + props.linkedin}
+              target="_blank"
+            >
               linkedin
             </a>
           </h4>

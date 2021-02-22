aa8babc2c539540b81f0fb5740b200c5449c556b
Yousef Ahmed <yousefahmed0066@gmail.com>
2021-02-22 17:22:43 -0600

    Update script location

--- backend/scripts.py
+++ backend/scripts.py
@@ -1,5 +1,3 @@
-__version__ = '0.1.0'
-
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from api import create_app
@@ -14,18 +12,12 @@ migrate = Migrate(app, db)
 # adds the python manage.py db init, db migrate, db upgrade commands
 manager.add_command("db", MigrateCommand)
 
-
-@manager.command
 def runserver():
     app.run(debug=True, host="0.0.0.0", port=5000)
 
-
-@manager.command
 def runprod():
     app.run(debug=True, host="0.0.0.0")
 
-
-@manager.command
 def runworker():
     app.run(debug=False)
 

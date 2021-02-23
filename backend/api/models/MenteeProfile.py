a3c3a1c70d134529bf97dab80c7d6f7fe613006a
Leundai <batmanlup2@gmail.com>
2021-02-22 18:55:27 -0600

    add menteeprofile with mentorprofile fields

---
+++ backend/api/models/MenteeProfile.py
@@ -0,0 +1,36 @@
+from api.core import Mixin
+from .base import db
+from flask_mongoengine import Document
+from mongoengine import *
+from api.models import Education, Video, Availability, Image, Users
+
+
+class MenteeProfile(Document, Mixin):
+    """"Mentee Profile Collection."""
+
+    name = StringField(required=True)
+    location = StringField()
+    email = StringField(required=True)
+    phone_number = StringField()
+    professional_title = StringField(required=True)
+    linkedin = StringField()
+    website = StringField()
+    image = EmbeddedDocumentField(Image)
+    education = ListField(EmbeddedDocumentField(Education))
+    languages = ListField(StringField(), required=True)
+    specializations = ListField(StringField(), required=True)
+    biography = StringField(required=False)
+    offers_in_person = BooleanField(required=True)
+    offers_group_appointments = BooleanField(required=True)
+    videos = ListField(EmbeddedDocumentField(Video))
+    availability = ListField(EmbeddedDocumentField(Availability))
+    text_notifications = BooleanField(required=True)
+    email_notifications = BooleanField(required=True)
+
+    def __repr__(self):
+        return f"""<MenteeProfile user_id:{self.id} \n name: {self.name} 
+                \n professional title: {self.professional_title} 
+                \n linkedin: {self.linkedin} \n website: {self.website}
+                \n image: {self.image} \n biography: {self.biography} 
+                \n offers_in_person: {self.offers_in_person} 
+                \n offers_group_appointments: {self.offers_group_appointments}>"""

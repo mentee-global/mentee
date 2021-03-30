9575cd90a2559a94f4888a0fcc321c3ede181eaa
leundai <batmanlup2@gmail.com>
2021-03-29 19:28:26 -0500

    credit

--- frontend/src/utils/hooks/usePersistedState.js
+++ frontend/src/utils/hooks/usePersistedState.js
@@ -1,5 +1,8 @@
 import React, { useEffect } from "react";
 
+// Bless this person...
+// Source:
+// https://dev.to/selbekk/persisting-your-react-state-in-9-lines-of-code-9go
 export default function usePersistedState(key, defaultValue) {
   const [state, setState] = React.useState(
     () => JSON.parse(localStorage.getItem(key)) || defaultValue

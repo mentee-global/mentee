a8e1ccca61f1030d87472cf3b06347c0aa9ed692
Feiyu Wong <feiyucw2@illinois.edu>
2021-03-29 19:06:20 -0500

    added new endpoints to get specific messages

--- backend/api/views/messages.py
+++ backend/api/views/messages.py
@@ -18,7 +18,47 @@ def get_all_messages():
         msg = "Failed to get messages"
         logger.info(msg)
         return create_response(status=422, message=msg)
+    return create_response(data={"Messages": messages}, status=200, message="Success")
 
+@messages.route("/from=<string:sender_username>", methods=["GET"])
+def get_all_messages_from_user(sender_username):
+    try:
+        messages = Message.objects.get(user_name=sender_username)
+    except:
+        msg = "Failed to get messages"
+        logger.info(msg)
+        return create_response(status=422, message=msg)
+    return create_response(data={"Messages": messages}, status=200, message="Success")
+
+@messages.route("/from_id=<string:sender_id>", methods=["GET"])
+def get_all_messages_from_user_id(sender_id):
+    try:
+        messages = Message.objects.get(user_id=sender_id)
+    except:
+        msg = "Failed to get messages"
+        logger.info(msg)
+        return create_response(status=422, message=msg)
+    return create_response(data={"Messages": messages}, status=200, message="Success")
+
+@messages.route("/to=<string:recipient_username>", methods=["GET"])
+def get_all_messages_to_user(recipient_username):
+    try:
+        messages = Message.objects.get(recipient_name=recipient_username)
+    except:
+        msg = "Failed to get messages"
+        logger.info(msg)
+        return create_response(status=422, message=msg)
+    return create_response(data={"Messages": messages}, status=200, message="Success")
+
+@messages.route("/to_id=<string:recipient_id>", methods=["GET"])
+def get_all_messages_to_user_id(recipient_id):
+    try:
+        messages = Message.objects.get(recipient_id=recipient_id)
+    except:
+        msg = "Failed to get messages"
+        logger.info(msg)
+        return create_response(status=422, message=msg)
+    return create_response(data={"Messages": messages}, status=200, message="Success")
 
 @messages.route("/", methods=["POST"])
 def create_message():
@@ -45,6 +85,6 @@ def create_message():
         msg = "Failed to save message"
         logger.info(msg)
         return create_response(status=422, message=msg)
-    return create_response(
+    return create_response(status=201,
         message=f"Successfully saved message: {message.message} from user: {message.user_name} to: {message.recipient_name} sent on: {message.time}"
     )

ec5e2865f7d5c42b216fd1e56de23b54d73ea9bf
leundai <batmanlup2@gmail.com>
2021-04-01 19:35:47 -0500

    menteesidebar formatting

--- frontend/src/components/MenteeSidebar.js
+++ frontend/src/components/MenteeSidebar.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function MenteeSideBar() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return <div></div>;
 }
 
-export default MenteeSideBar
+export default MenteeSideBar;

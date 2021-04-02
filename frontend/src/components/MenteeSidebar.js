d185d40acbe200f05ceec3c1dc64f17e91ff9bb2
leundai <batmanlup2@gmail.com>
2021-04-01 20:00:37 -0500

    Revert "menteesidebar formatting"
    This reverts commit ec5e2865f7d5c42b216fd1e56de23b54d73ea9bf.

--- frontend/src/components/MenteeSidebar.js
+++ frontend/src/components/MenteeSidebar.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react'
 
 function MenteeSideBar() {
-  return <div></div>;
+    return (
+        <div>
+            
+        </div>
+    )
 }
 
-export default MenteeSideBar;
+export default MenteeSideBar

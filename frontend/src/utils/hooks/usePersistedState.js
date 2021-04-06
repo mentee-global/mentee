bafd28e378c206307caa4edfb2250911effc2324
leundai <batmanlup2@gmail.com>
2021-04-05 19:18:20 -0500

    fixed usePersistedState

--- frontend/src/utils/hooks/usePersistedState.js
+++ frontend/src/utils/hooks/usePersistedState.js
@@ -10,9 +10,10 @@ import React, { useEffect } from "react";
 // Source:
 // https://dev.to/selbekk/persisting-your-react-state-in-9-lines-of-code-9go
 export default function usePersistedState(key, defaultValue) {
-  const [state, setState] = React.useState(
-    () => JSON.parse(localStorage.getItem(key)) || defaultValue
-  );
+  const [state, setState] = React.useState(() => {
+    const persisted = JSON.parse(localStorage.getItem(key));
+    return persisted != null ? persisted : defaultValue;
+  });
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(state));
   }, [key, state]);

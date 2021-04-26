bc25db6aceb83ad828867338f440864d59817801
Feiyu Wong <feiyucw2@illinois.edu>
2021-04-26 18:44:41 -0500

    saved changes in mentee Footer

--- frontend/src/components/MenteeFooter.js
+++ frontend/src/components/MenteeFooter.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { NavLink, useHistory } from "react-router-dom";
 import { logout, getMentorID, getAdminID } from "utils/auth.service";
 import { Layout, Dropdown, Menu } from "antd";
+import MenteeMessageTab from "MenteeMessageTab";
 
 
 const { Footer } = Layout;
@@ -9,57 +10,10 @@ const { Footer } = Layout;
 function MenteeFooter() {
   const history = useHistory();
 
-  const [admin, setAdmin] = useState();
-
-  useEffect(() => {
-    async function fetchData() {
-      const adminId = await getAdminID();
-      const admin = await getAdmin(adminId);
-
-      if (admin) {
-        setAdmin(admin);
-      }
-    }
-    fetchData();
-  }, []);
-
-  const dropdownMenu = (
-    <Menu className="dropdown-menu">
-      <Menu.Item
-        key="sign-out"
-        onClick={() => logout().then(() => history.push("/"))}
-      >
-        <b>Sign Out</b>
-      </Menu.Item>
-    </Menu>
-  );
-
   return (
-    <Header className="navigation-header">
-      <div>
-        <NavLink to="/">
-          <img
-            src={isMobile ? MenteeLogoSmall : MenteeLogo}
-            alt="Mentee"
-            className="mentee-logo"
-          />
-        </NavLink>
-      </div>
-      <span>
-        <div className="profile-caret">
-          <Dropdown overlay={dropdownMenu} trigger={["click"]}>
-            <CaretDownOutlined />
-          </Dropdown>
-        </div>
-        {admin && (
-          <>
-            <div className="profile-name">
-              <b>{admin.name}</b>
-            </div>
-          </>
-        )}
-      </span>
-    </Header>
+    <Footer>
+      <MenteeMessageTab></MenteeMessageTab>
+    </Footer>
   );
 }
 

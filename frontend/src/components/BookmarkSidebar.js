c3b69bb20e64e94ca02e6955be53ddb975cf6f74
leundai <batmanlup2@gmail.com>
2021-05-20 20:48:47 -0500

    add spinning feedback

--- frontend/src/components/BookmarkSidebar.js
+++ frontend/src/components/BookmarkSidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Avatar } from "antd";
+import { Avatar, Spin } from "antd";
 import { UserOutlined, HeartFilled } from "@ant-design/icons";
 
 import MenteeButton from "./MenteeButton";
@@ -29,33 +29,38 @@ function BookmarkSidebar({ bookmarks, unfavorite }) {
         <HeartFilled /> Favorite Contacts
       </div>
       <div className="mentee-bookmark-display">
-        {bookmarks.map((mentor) => (
-          <div className="mentee-bookmark-card">
-            <Avatar
-              src={mentor.image && mentor.image.url}
-              icon={<UserOutlined />}
-              size={30}
-              style={{ minWidth: "30px" }}
-            />
-            <div className="mentee-bookmark-mentor-info">
-              <HeartFilled
-                className="bookmark-heart"
-                onClick={() => unfavorite(mentor.id, mentor.name)}
+        <Spin
+          spinning={!bookmarks || !bookmarks.length}
+          className="bookmark-spin"
+        >
+          {bookmarks.map((mentor) => (
+            <div className="mentee-bookmark-card">
+              <Avatar
+                src={mentor.image && mentor.image.url}
+                icon={<UserOutlined />}
+                size={30}
+                style={{ minWidth: "30px" }}
               />
-              <div>
-                <div
-                  className="mentee-bookmark-mentor-name"
-                  onClick={() => redirectToProfile(mentor.id)}
-                >
-                  {mentor.name}
-                </div>
-                <div className="mentee-bookmark-mentor-title">
-                  {mentor.professional_title}
+              <div className="mentee-bookmark-mentor-info">
+                <HeartFilled
+                  className="bookmark-heart"
+                  onClick={() => unfavorite(mentor.id, mentor.name)}
+                />
+                <div>
+                  <div
+                    className="mentee-bookmark-mentor-name"
+                    onClick={() => redirectToProfile(mentor.id)}
+                  >
+                    {mentor.name}
+                  </div>
+                  <div className="mentee-bookmark-mentor-title">
+                    {mentor.professional_title}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </Spin>
       </div>
     </div>
   );

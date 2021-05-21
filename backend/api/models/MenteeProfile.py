a1614f74cf85307a094ef73b84c3881d4db3d52b
leundai <batmanlup2@gmail.com>
2021-05-21 17:36:22 -0500

    removed unused field

--- backend/api/models/MenteeProfile.py
+++ backend/api/models/MenteeProfile.py
@@ -9,7 +9,6 @@ class MenteeProfile(Document, Mixin):
     """Mentee Profile Collection."""
 
     firebase_uid = StringField()
-    user_id = ReferenceField("Users")
     name = StringField(required=True)
     gender = StringField(required=True)
     location = StringField()

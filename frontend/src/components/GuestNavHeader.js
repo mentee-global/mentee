fd180129aea007d68ee45976f6feb8cde530b06f
Zayyan Faizal <zfaizal2@illinois.edu>
2021-09-29 21:07:30 -0500

    fixed bugs on admin nav header and updated styling

--- frontend/src/components/GuestNavHeader.js
+++ frontend/src/components/GuestNavHeader.js
@@ -7,11 +7,11 @@ import { withRouter } from "react-router-dom";
 import { isLoggedIn } from "utils/auth.service";
 import MenteeButton from "./MenteeButton";
 import LoginVerificationModal from "./LoginVerificationModal";
-import { fetchAccountById } from "utils/api";
+import { fetchAccountById, getAdmin } from "utils/api";
 import useAuth from "../utils/hooks/useAuth";
 import { ACCOUNT_TYPE } from "utils/consts";
 import "./css/Navigation.scss";
-
+import { getAdminID } from "utils/auth.service";
 import MenteeLogo from "../resources/mentee.png";
 import MenteeLogoSmall from "../resources/menteeSmall.png";
 import Icon, { UserOutlined, MenuOutlined, CaretDownOutlined } from "@ant-design/icons";
@@ -26,6 +26,26 @@ function GuestNavHeader({ history }) {
 
   const [user, setUser] = useState();
 
+  useEffect(() => {
+    async function getUser() {
+      if (role == ACCOUNT_TYPE.ADMIN) {
+        const adminId = await getAdminID();
+        const admin = await getAdmin(adminId);
+        if (admin) {
+          setUser(admin)
+        }
+      } else {
+        const userData = await fetchAccountById(profileId, role);
+        if (userData) {
+          setUser(userData);
+        }
+      }
+    }
+    // Don't fetch if guest
+    if (role == ACCOUNT_TYPE.GUEST || user) return;
+    onAuthStateChanged(getUser);
+  }, [role]);
+
   const logoutUser = () => {
     logout().then(() => {
       resetRoleState();
@@ -33,18 +53,7 @@ function GuestNavHeader({ history }) {
     });
   };
 
-  useEffect(() => {
-    async function getUser() {
-      const userData = await fetchAccountById(profileId, role);
-      if (userData) {
-        setUser(userData);
-      }
-    }
 
-    // Don't fetch if guest
-    if (role == ACCOUNT_TYPE.GUEST || user) return;
-    onAuthStateChanged(getUser);
-  }, [role]);
     const dropdownMenu = (
       <Menu className="dropdown-menu">
         <Menu.Item key="edit-profile">
@@ -79,7 +88,7 @@ function GuestNavHeader({ history }) {
           <div style={{ display: "flex" }}>
             {!isMobile && (
               <>
-                {isMentee || isMentor || isAdmin ?
+                {isMentee || isMentor ?
                 <></> :
                 <span className="navigation-header-button">
                   <MenteeButton
@@ -97,7 +106,7 @@ function GuestNavHeader({ history }) {
               </>
             )}
             {/* TODO: Update this since verification modal will not longer be needed anymore! */}
-            { isMentor || isAdmin ?
+            { isMentor ?
               <></> :
               <span className="navigation-header-button">
                 <LoginVerificationModal
@@ -113,7 +122,7 @@ function GuestNavHeader({ history }) {
                 />
               </span>
             }
-            { isMentee || isAdmin ?
+            { isMentee ?
               <></> :
               <span className="navigation-header-button">
                 <LoginVerificationModal
@@ -149,6 +158,28 @@ function GuestNavHeader({ history }) {
                 }}
               />
             </span> 
+            {user ?
+              <>
+                <div className="profile-name">
+                  <b>{user.name}</b>
+                  <br />
+                  {user.professional_title}
+                </div>
+                <div className="profile-picture">
+                  <Avatar
+                    size={40}
+                    src={user.image && user.image.url}
+                    icon={<UserOutlined />}
+                  />
+                </div>
+                <div className="profile-caret">
+                  <Dropdown overlay={dropdownMenu} trigger={["click"]}>
+                    <CaretDownOutlined />
+                  </Dropdown>
+                </div>
+                </> :
+                <></>
+              }
           </div>
         ) : (
           <MobileGuestNavHeader
@@ -157,28 +188,7 @@ function GuestNavHeader({ history }) {
             history={history}
           />
         )}
-        {user ?
-          <>
-            <div className="profile-name">
-              <b>{user.name}</b>
-              <br />
-              {user.professional_title}
-            </div>
-            <div className="profile-picture">
-              <Avatar
-                size={40}
-                src={user.image && user.image.url}
-                icon={<UserOutlined />}
-              />
-            </div>
-            <div className="profile-caret">
-            <Dropdown overlay={dropdownMenu} trigger={["click"]}>
-              <CaretDownOutlined />
-            </Dropdown>
-            </div>
-            </> :
-            <></>
-        }
+
       </div>
     </Header>
   );
@@ -213,7 +223,7 @@ function MobileGuestNavHeader({ setDrawerVisible, drawerVisible, history }) {
             }}
           />
           </div>
-          {isMentor || isAdmin ?
+          { isMentor ?
             <></> :
             <LoginVerificationModal
               className="mobile-nav-btn-login-modal"
@@ -228,7 +238,7 @@ function MobileGuestNavHeader({ setDrawerVisible, drawerVisible, history }) {
               }}
             /> 
           }
-          { isMentee || isAdmin ?
+          { isMentee ?
           <></> :
           <LoginVerificationModal
             className="mobile-nav-btn-login-modal"

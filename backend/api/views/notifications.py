dd31862810c3f63f5ce7dc609a1df0c6e8dfe453
lucianatl <lucianat@illinois.edu>
2021-11-30 20:36:39 -0500

    endpoint done no testing

--- backend/api/views/notifications.py
+++ backend/api/views/notifications.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from api.core import create_response, logger
 from flask import Blueprint
+from backend.api.models.MenteeProfile import MenteeProfile, MentorProfile
 from mongoengine.queryset.visitor import Q
 from api.models import DirectMessage
 from api.utils.request_utils import send_email
@@ -23,13 +24,50 @@ def get_unread_dm_count(id):
     return create_response(data={"notifications": notifications})
 
 
-@notifications.route("/<id>", methods=["GET"])
-def send_weekly_email(user, notifications):
-    res, res_msg = send_email(
-        recipient=user.email,
-        template_id=WEEKLY_NOTIF_REMINDER,
-        data={"notification_count": notifications},
-    )
-    if not res:
-        msg = "Failed to send mentee email " + res_msg
-        logger.info(msg)
+@notifications.route("/weeklyemails", methods=["GET"])
+def send_weekly_emails():
+
+    try:
+        mentee_users = MenteeProfile.objects()
+        mentor_users = MentorProfile.objects()
+    except Exception as e:
+        msg = "error"
+        logger.info(e)
+        return create_response(status=422, message=msg)
+    for user in mentee_users:
+        try:
+            notifications = DirectMessage.objects(
+                Q(recipient_id=user._id) & Q(message_read=False)
+            ).count()
+        except Exception as e:
+            msg = "No mentee with that id"
+            logger.info(e)
+            return create_response(status=422, message=msg)
+        if notifications > 0:
+            res, res_msg = send_email(
+                recipient=user.email,
+                template_id=WEEKLY_NOTIF_REMINDER,
+                data={"number_unread": notifications},
+            )
+            if not res:
+                msg = "Failed to send mentee email " + res_msg
+                logger.info(msg)
+
+    for user in mentor_users:
+        try:
+            notifications = DirectMessage.objects(
+                Q(recipient_id=user._id) & Q(message_read=False)
+            ).count()
+        except Exception as e:
+            msg = "No mentor with that id"
+            logger.info(e)
+            return create_response(status=422, message=msg)
+        if notifications > 0:
+            res, res_msg = send_email(
+                recipient=user.email,
+                template_id=WEEKLY_NOTIF_REMINDER,
+                data={"number_unread": notifications},
+            )
+            if not res:
+                msg = "Failed to send mentee email " + res_msg
+                logger.info(msg)
--- backend/scripts/emails.py
+++ backend/scripts/emails.py
@@ -1,12 +1,14 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import requests
 
 sched = BlockingScheduler()
 
 
 @sched.scheduled_job('cron', day_of_week='mon', hour=17)
 def scheduled_job():
-
-    print('This job is run every monday at 5pm.')
+    # python requests
+    r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+    print(r.status_code)
 
 
 sched.start()

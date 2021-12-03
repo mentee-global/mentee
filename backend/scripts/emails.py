32e922e58ebea8a929c3299bf20818b6d103770a
leundai <batmanlup2@gmail.com>
2021-12-02 19:17:22 -0600

    formatting and updating script.py

--- backend/scripts/emails.py
+++ backend/scripts/emails.py
@@ -4,10 +4,10 @@ import requests
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', day_of_week='mon', hour=12)
+@sched.scheduled_job("cron", day_of_week="mon", hour=12)
 def scheduled_job():
     # python requests
-    r = requests.get('localhost:5000/api/notifications/weeklyemails')
+    r = requests.get("localhost:5000/api/notifications/weeklyemails")
     print(r.status_code)
 
 

347740976787f249bae3efa1193e82c8c8112a1c
lucianatl <lucianat@illinois.edu>
2021-12-04 00:03:32 -0600

    adding actaul file lmaoa

---
+++ frontend/src/features/notificationsSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import {get_unread_dm_count } from "backend/views/notifications.py";
+
+export const fetchNotificationsCount = createAsyncThunk(
+  "fetchNotificationsCount",
+  async ({ id }) => {
+    const res = await get_unread_dm_count(id);
+    return res;
+  }
+);
+
+export const notificationsSlice = createSlice({
+  name: "notifications",
+  initialState: {
+    count: 0,
+    status: "idle"
+  },
+  reducers: {
+    resetNotifications(state, action) {
+      state.count = 0;
+    },
+    decrement: (state, action) => state.count - action.payload,
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchNotificationsCount.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchNotificationsCount.fulfilled, (state, action) => {
+        state.count = action.payload;
+        state.status = "succeeded";
+      })
+  },
+});
+
+export const { resetNotifications, decrement } = notificationsSlice.actions;
+
+export default notificationsSlice.reducer;

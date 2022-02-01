fcfb6cf82e9a5bda7847efe44f8dfb42c69c44ce
RavishingRitesh <97825308+RavishingRitesh@users.noreply.github.com>
2022-02-02 01:22:34 +0530

    Update UploadEmails.js
    Changes for saving verified emails in bulk.

--- frontend/src/components/UploadEmails.js
+++ frontend/src/components/UploadEmails.js
@@ -1,12 +1,33 @@
 import React, { useCallback, useState } from "react";
-import { Modal, Form, Input, Button, message } from "antd";
+import { Modal, Form, Input, Button, message, Radio } from "antd";
 import { useDropzone } from "react-dropzone";
-import { adminUploadEmails } from "utils/api";
+import { adminUploadEmails, adminUploadEmailsText } from "utils/api";
 import MenteeButton from "./MenteeButton";
 
 import "./css/UploadEmails.scss";
 
 function UploadEmails(props) {
+  const { TextArea } = Input;
+  const onChange5 = (e) => setCompanyTime(e.target.value);
+  const [companyTime, setCompanyTime] = useState();
+  const [messageText, setMessageText] = useState("");
+
+  const onFinish = useCallback((messageText, companyTime) => {
+    async function uploadEmailsText(messageText) {
+      await adminUploadEmailsText(messageText, companyTime);
+    }
+
+    uploadEmailsText(messageText);
+
+    setMessageText("");
+    setCompanyTime("");
+    success();
+  }, []);
+
+  const success = () => {
+    message.success("This is a success message");
+  };
+
   return (
     <Modal
       visible={props.uploadModalVisible}
@@ -23,93 +44,38 @@ function UploadEmails(props) {
     >
       {" "}
       <div className="dragdrops">
-        <div className="dragdrop">
-          <h3>Add Mentors</h3>
-          {DragDrop(true)}
-        </div>
-        <div className="dragdrop">
-          <h3>Add Mentees</h3>
-          {DragDrop(false)}
+        <h1>Add Bulk Users</h1>
+        <h2>
+          Profile to add :
+          <Radio.Group onChange={onChange5} value={companyTime}>
+            <Radio value={"true"}>Mentor</Radio>
+            <Radio value={"false"}>Mentee</Radio>
+          </Radio.Group>
+        </h2>
+        <h4>
+          Enter multiple email addresses,seperated by semicolon ';' then submit
+        </h4>
+        <div>
+          <Form onFinish={() => onFinish(messageText, companyTime)}>
+            <Form.Item>
+              <TextArea
+                className="message-input"
+                placeholder="Enter email(s)"
+                value={messageText}
+                onChange={(e) => setMessageText(e.target.value)}
+                autoSize={{ minRows: 4, maxRows: 10 }}
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
         </div>
       </div>
     </Modal>
-  );
-}
-
-function DragDrop(isMentor) {
-  const [password, setPassword] = useState("");
-  const [files, setFiles] = useState([]);
-
-  const onDrop = (acceptedFiles) => {
-    setFiles(acceptedFiles);
-  };
-
-  const file_names = files.map((file) => <li> {file.path} </li>);
-
-  const onFinish = useCallback((files, password, mentor) => {
-    async function uploadEmails(file) {
-      await adminUploadEmails(file, password, mentor);
-    }
-    files.forEach((file) => {
-      uploadEmails(file);
-    });
-    setFiles([]);
-    setPassword("");
-    success();
-  }, []);
-
-  const success = () => {
-    message.success("This is a success message");
-  };
-
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop,
-    accept: ".csv",
-  });
-
-  const includePassword = () => {
-    if (isMentor) {
-      return <div></div>;
-    } else {
-      return (
-        <Form.Item label="Password">
-          <Input
-            name="inputtedPassword"
-            type="text"
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-        </Form.Item>
-      );
-    }
-  };
-  //
-  return (
-    <div>
-      <Form
-        onFinish={() => onFinish(files, password, isMentor)}
-        initialValues={{ inputtedPassword: "" }}
-      >
-        <Form.Item>
-          <div {...getRootProps()}>
-            <p>Drag 'n' drop some files here, or click to select files</p>
-            <em>(Only *.csv files will be accepted)</em>
-            <input {...getInputProps()} />
-          </div>
-          <aside>
-            <h4>Uploaded Files: </h4>
-            <ul type="circle">{file_names}</ul>
-          </aside>
-        </Form.Item>
-        {includePassword()}
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
   );
 }
 

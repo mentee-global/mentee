159eee580d19eb603befc0f6bcc15a6f9abbc487
eslamzaaki <solomzaki840@gmail.com>
2022-04-30 18:34:59 -0700

    🍐 Apply login partner changes (#506)
    * change login and apply logic
    
    * Add mentor application work in front and back end
    
    * add apply process back and front end for 3 roles
    
    * stage 1 ready for test
    
    * :
    
    * enhance UI finish stage 1
    
    * stage 1
    
    * stage 1.
    
    * stage 1..
    
    * ehance UI stage1
    
    * fix login and apply UI
    
    * fix login and apply UI
    
    * fix login and apply UI.
    
    * fix mentee button
    
    * stage 2 ready for test
    
    * fix stage2 issues
    
    * stage2 fixes
    
    * stage2 changes add Training CMS && new Emails

--- backend/api/utils/profile_parse.py
+++ backend/api/utils/profile_parse.py
@@ -1,3 +1,4 @@
+from api.models.PartnerProfile import PartnerProfile
 from bson import ObjectId
 from api.core import logger
 from api.models import db, Education, Video, MentorProfile, MenteeProfile, Image
@@ -19,8 +20,28 @@ def new_profile(data: dict = {}, profile_type: int = -1):
         return None
 
     new_profile = None
+    if profile_type==Account.PARTNER:
+        new_profile = PartnerProfile(
+            firebase_uid=data["firebase_uid"],
+            email=data["email"],
+            organization=data["organization"],
+            location=data["location"],
+            regions=data["regions"],
+            intro=data["intro"],
+            open_grants=data.get('open_grants'),
+            open_projects=data.get('open_projects'),
+            topics=data.get('topics'),
+            sdgs=data.get('sdgs'),
+            email_notifications=data.get("email_notifications", True),
+            text_notifications=data.get("text_notifications", False),
+            
+        )
+        new_profile.website = data.get("website")
+        new_profile.linkedin = data.get("linkedin")
+        new_profile.person_name=data.get('person_name')
+        return new_profile
 
-    if profile_type == Account.MENTOR:
+    elif profile_type == Account.MENTOR:
         new_profile = MentorProfile(
             firebase_uid=data["firebase_uid"],
             name=data["name"],
@@ -32,10 +53,12 @@ def new_profile(data: dict = {}, profile_type: int = -1):
             email_notifications=data.get("email_notifications", True),
             text_notifications=data.get("text_notifications", False),
             taking_appointments=data.get("taking_appointments", False),
+            
         )
 
         new_profile.website = data.get("website")
         new_profile.linkedin = data.get("linkedin")
+        new_profile.video=data.get('video')
 
         if "videos" in data:
             video_data = data.get("videos")
@@ -119,9 +142,19 @@ def edit_profile(data: dict = {}, profile: object = None):
         profile.offers_in_person = data.get(
             "offers_in_person", profile.offers_in_person
         )
+        profile.taking_appointments = data.get(
+        "taking_appointments", profile.taking_appointments
+        )
         profile.linkedin = data.get("linkedin", profile.linkedin)
         profile.website = data.get("website", profile.website)
-
+        if "video" in data:
+            video_data = data.get("video")
+            profile.video = Video(
+                title=video_data.get("title"),
+                url=video_data.get("url"),
+                tag=video_data.get("tag"),
+                date_uploaded=video_data.get("date_uploaded"),
+            )
         # Create video objects for each item in list
         if "videos" in data:
             video_data = data.get("videos")
@@ -139,6 +172,8 @@ def edit_profile(data: dict = {}, profile: object = None):
         profile.gender = data.get("gender", profile.gender)
         profile.organization = data.get("organization", profile.organization)
         profile.is_private = data.get("is_private", profile.is_private)
+        profile.specializations = data.get("specializations", profile.specializations)
+
 
         if "video" in data:
             video_data = data.get("video")
@@ -155,9 +190,6 @@ def edit_profile(data: dict = {}, profile: object = None):
     profile.phone_number = data.get("phone_number", profile.phone_number)
     profile.languages = data.get("languages", profile.languages)
     profile.biography = data.get("biography", profile.biography)
-    profile.taking_appointments = data.get(
-        "taking_appointments", profile.taking_appointments
-    )
     profile.text_notifications = data.get(
         "text_notifications", profile.text_notifications
     )
--- frontend/src/components/pages/ApplicationOrganizer.js
+++ frontend/src/components/pages/ApplicationOrganizer.js
@@ -4,251 +4,306 @@ import { Modal } from "antd";
 import { ExclamationCircleOutlined } from "@ant-design/icons";
 import { fetchApplications, updateApplicationById } from "../../utils/api";
 import MentorApplicationView from "../MentorApplicationView";
-import { APP_STATUS } from "../../utils/consts";
+import { APP_STATUS, NEW_APPLICATION_STATUS } from "../../utils/consts";
 import useAuth from "utils/hooks/useAuth";
 
 const { confirm } = Modal;
 
-function ApplicationOrganizer() {
-  const { onAuthStateChanged } = useAuth();
-  const [applicationData, setApplicationData] = useState([]);
-  const [columns, setColumns] = useState({
-    [1]: {
-      name: "Pending",
-      items: [],
-    },
-    [2]: {
-      name: "Reviewed",
-      items: [],
-    },
-    [3]: {
-      name: "Rejected",
-      items: [],
-    },
-    [4]: {
-      name: "Offer Made",
-      items: [],
-    },
-  });
+function ApplicationOrganizer({ isMentor }) {
+	const { onAuthStateChanged } = useAuth();
+	const [applicationData, setApplicationData] = useState([]);
+	const [columns, setColumns] = useState({
+		[1]: {
+			name: NEW_APPLICATION_STATUS.PENDING,
+			items: [],
+		},
+		[2]: {
+			name: NEW_APPLICATION_STATUS.APPROVED,
+			items: [],
+		},
+		[3]: {
+			name: NEW_APPLICATION_STATUS.BUILDPROFILE,
+			items: [],
+		},
+		[4]: {
+			name: NEW_APPLICATION_STATUS.COMPLETED,
+			items: [],
+		},
+		[5]: {
+			name: NEW_APPLICATION_STATUS.REJECTED,
+			items: [],
+		},
+	});
 
-  useEffect(() => {
-    const getAllApplications = async () => {
-      const applications = await fetchApplications();
-      if (applications) {
-        const newApplications = applications.mentor_applications.map(
-          (app, index) => {
-            return {
-              ...app,
-              index: index,
-            };
-          }
-        );
-        setApplicationData(newApplications);
-      }
-    };
+	useEffect(() => {
+		const getAllApplications = async () => {
+			const applications = await fetchApplications(isMentor);
+			if (applications) {
+				const newApplications = applications.mentor_applications.map(
+					(app, index) => {
+						return {
+							...app,
+							index: index,
+						};
+					}
+				);
+				setApplicationData(newApplications);
+			}
+		};
 
-    onAuthStateChanged(getAllApplications);
-  }, []);
+		onAuthStateChanged(getAllApplications);
+	}, []);
 
-  useEffect(() => {
-    setColumns({
-      [1]: {
-        name: "Pending",
-        items: filterApplications(APP_STATUS.PENDING),
-      },
-      [2]: {
-        name: "Reviewed",
-        items: filterApplications(APP_STATUS.REVIEWED),
-      },
-      [3]: {
-        name: "Rejected",
-        items: filterApplications(APP_STATUS.REJECTED),
-      },
-      [4]: {
-        name: "Offer Made",
-        items: filterApplications(APP_STATUS.OFFER_MADE),
-      },
-    });
-  }, [applicationData]);
+	useEffect(() => {
+		setColumns({
+			[1]: {
+				name: NEW_APPLICATION_STATUS.PENDING,
+				items: filterApplications(NEW_APPLICATION_STATUS.PENDING),
+			},
+			[2]: {
+				name: NEW_APPLICATION_STATUS.APPROVED,
+				items: filterApplications(NEW_APPLICATION_STATUS.APPROVED),
+			},
+			[3]: {
+				name: NEW_APPLICATION_STATUS.BUILDPROFILE,
+				items: filterApplications(NEW_APPLICATION_STATUS.BUILDPROFILE),
+			},
+			[4]: {
+				name: NEW_APPLICATION_STATUS.COMPLETED,
+				items: filterApplications(NEW_APPLICATION_STATUS.COMPLETED),
+			},
+			[5]: {
+				name: NEW_APPLICATION_STATUS.REJECTED,
+				items: filterApplications(NEW_APPLICATION_STATUS.REJECTED),
+			},
+		});
+	}, [applicationData]);
 
-  /**
-   * Filters application by application state and items stored in the corresponding named columns
-   */
-  function filterApplications(desiredState) {
-    return applicationData
-      .filter(
-        (state) =>
-          state.application_state &&
-          state.application_state.toLowerCase() === desiredState.toLowerCase()
-      )
-      .map((application) => ({
-        id: application._id.$oid,
-        content:
-          "Name: " +
-          application.name +
-          " Specializations: " +
-          application.specializations,
-        ...application,
-      }));
-  }
+	const updateApps = async () => {
+		const applications = await fetchApplications(isMentor);
+		if (applications) {
+			const newApplications = applications.mentor_applications.map(
+				(app, index) => {
+					return {
+						...app,
+						index: index,
+					};
+				}
+			);
+			setApplicationData(newApplications);
+			setColumns({
+				[1]: {
+					name: NEW_APPLICATION_STATUS.PENDING,
+					items: filterApplications(NEW_APPLICATION_STATUS.PENDING),
+				},
+				[2]: {
+					name: NEW_APPLICATION_STATUS.APPROVED,
+					items: filterApplications(NEW_APPLICATION_STATUS.APPROVED),
+				},
+				[3]: {
+					name: NEW_APPLICATION_STATUS.BUILDPROFILE,
+					items: filterApplications(NEW_APPLICATION_STATUS.BUILDPROFILE),
+				},
+				[4]: {
+					name: NEW_APPLICATION_STATUS.COMPLETED,
+					items: filterApplications(NEW_APPLICATION_STATUS.COMPLETED),
+				},
+				[5]: {
+					name: NEW_APPLICATION_STATUS.REJECTED,
+					items: filterApplications(NEW_APPLICATION_STATUS.REJECTED),
+				},
+			});
+		}
+	};
+	/**
+	 * Filters application by application state and items stored in the corresponding named columns
+	 */
+	function filterApplications(desiredState) {
+		return applicationData
+			.filter(
+				(state) =>
+					state.application_state &&
+					state.application_state.toLowerCase() === desiredState.toLowerCase()
+			)
+			.map((application) => ({
+				id: application._id.$oid,
+				content: {
+					name: `Name: ${application.name}`,
+					email: `Email: ${application.email}`,
+				},
+				...application,
+			}));
+	}
 
-  /**
-   * Confirmation modal when item is dragged to a new column destination
-   */
-  function showConfirm(name, id, removed, sourceItems, sourceColumn, sourceID) {
-    confirm({
-      title: "Move this Application?",
-      icon: <ExclamationCircleOutlined />,
-      onOk() {
-        // updates application state of item based on destination column name
-        async function updateApplication() {
-          // onOk send the put request
-          const data = {
-            application_state: name,
-          };
-          await updateApplicationById(data, id);
-        }
-        updateApplication();
-      },
-      onCancel() {
-        // puts item back into source column if canceled
-        sourceItems.push(removed);
-        setColumns({
-          ...columns,
-          [sourceID]: {
-            ...sourceColumn,
-            items: sourceItems,
-          },
-        });
-      },
-    });
-  }
+	/**
+	 * Confirmation modal when item is dragged to a new column destination
+	 */
+	function showConfirm(name, id, removed, sourceItems, sourceColumn, sourceID) {
+		confirm({
+			title: "Move this Application?",
+			icon: <ExclamationCircleOutlined />,
+			onOk() {
+				// updates application state of item based on destination column name
+				async function updateApplication() {
+					// onOk send the put request
+					const data = {
+						application_state: name,
+					};
+					await updateApplicationById(data, id, isMentor);
+				}
+				updateApplication();
+			},
+			onCancel() {
+				// puts item back into source column if canceled
+				sourceItems.push(removed);
+				setColumns({
+					...columns,
+					[sourceID]: {
+						...sourceColumn,
+						items: sourceItems,
+					},
+				});
+			},
+		});
+	}
 
-  /**
-   * Handles dragging of item to a new column
-   */
-  const onDragEnd = (result, columns, setColumns) => {
-    // if no designated column to switch then keep app in curr column
-    if (!result.destination) return;
+	/**
+	 * Handles dragging of item to a new column
+	 */
+	const onDragEnd = (result, columns, setColumns) => {
+		// if no designated column to switch then keep app in curr column
+		if (!result.destination) return;
 
-    const { source, destination } = result;
-    if (source.droppableId !== destination.droppableId) {
-      const sourceColumn = columns[source.droppableId];
-      const destColumn = columns[destination.droppableId];
-      const sourceItems = [...sourceColumn.items];
-      const destItems = [...destColumn.items];
-      const [removed] = sourceItems.splice(source.index, 1);
-      destItems.splice(destination.index, 0, removed);
-      setColumns({
-        ...columns,
-        [source.droppableId]: {
-          ...sourceColumn,
-          items: sourceItems,
-        },
-        [destination.droppableId]: {
-          ...destColumn,
-          items: destItems,
-        },
-      });
+		const { source, destination } = result;
+		if (source.droppableId !== destination.droppableId) {
+			const sourceColumn = columns[source.droppableId];
+			const destColumn = columns[destination.droppableId];
+			const sourceItems = [...sourceColumn.items];
+			const destItems = [...destColumn.items];
+			const [removed] = sourceItems.splice(source.index, 1);
+			destItems.splice(destination.index, 0, removed);
+			setColumns({
+				...columns,
+				[source.droppableId]: {
+					...sourceColumn,
+					items: sourceItems,
+				},
+				[destination.droppableId]: {
+					...destColumn,
+					items: destItems,
+				},
+			});
 
-      var destColumnName = columns[destination.droppableId].name;
-      var destItemId = destItems[destination.index].id;
-      showConfirm(
-        destColumnName,
-        destItemId,
-        removed,
-        sourceItems,
-        sourceColumn,
-        source.droppableId
-      );
-    } else {
-      const column = columns[source.droppableId];
-      const copiedItems = [...column.items];
-      const [removed] = copiedItems.splice(source.index, 1);
-      copiedItems.splice(destination.index, 0, removed);
-      setColumns({
-        ...columns,
-        [source.droppableId]: {
-          ...column,
-          items: copiedItems,
-        },
-      });
-    }
-  };
+			var destColumnName = columns[destination.droppableId].name;
+			var destItemId = destItems[destination.index].id;
+			showConfirm(
+				destColumnName,
+				destItemId,
+				removed,
+				sourceItems,
+				sourceColumn,
+				source.droppableId
+			);
+		} else {
+			const column = columns[source.droppableId];
+			const copiedItems = [...column.items];
+			const [removed] = copiedItems.splice(source.index, 1);
+			copiedItems.splice(destination.index, 0, removed);
+			setColumns({
+				...columns,
+				[source.droppableId]: {
+					...column,
+					items: copiedItems,
+				},
+			});
+		}
+	};
 
-  return (
-    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
-      <DragDropContext
-        onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
-      >
-        {/* Mapping each columns from the list. */}
-        {Object.entries(columns).map(([columnId, column]) => {
-          return (
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-              key={columnId}
-            >
-              <h2>{column.name}</h2>
-              <div style={{ margin: 10 }}>
-                <Droppable droppableId={columnId} key={columnId}>
-                  {/* Causes the droppable item to change color of columm when picked up. */}
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				height: "100%",
+				overflowX: "auto",
+			}}
+		>
+			<DragDropContext
+				onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+			>
+				{/* Mapping each columns from the list. */}
+				{Object.entries(columns).map(([columnId, column]) => {
+					return (
+						<div
+							style={{
+								display: "flex",
+								flexDirection: "column",
+								alignItems: "center",
+							}}
+							key={columnId}
+						>
+							<h2>{column.name}</h2>
+							<div style={{ margin: 10 }}>
+								<Droppable droppableId={columnId} key={columnId}>
+									{/* Causes the droppable item to change color of columm when picked up. */}
 
-                  {(provided, currentApp) => {
-                    //printData()
-                    return (
-                      <div
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        style={{
-                          background: currentApp.isDraggingOver
-                            ? "#F8D15B"
-                            : "#F5F5F5",
-                          padding: 4,
-                          width: 250,
-                          minHeight: 500,
-                        }}
-                      >
-                        {/* Mapping each item from list that corresponds to the column */}
-                        {column.items.map((item, index) => {
-                          return (
-                            <Draggable
-                              key={item.id}
-                              draggableId={item.id}
-                              index={index}
-                            >
-                              {(provided) => {
-                                return (
-                                  <div
-                                    ref={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                    style={{
-                                      ...provided.draggableProps.style,
-                                    }}
-                                  >
-                                    <MentorApplicationView data={item} />
-                                  </div>
-                                );
-                              }}
-                            </Draggable>
-                          );
-                        })}
-                        {provided.placeholder}
-                      </div>
-                    );
-                  }}
-                </Droppable>
-              </div>
-            </div>
-          );
-        })}
-      </DragDropContext>
-    </div>
-  );
+									{(provided, currentApp) => {
+										//printData()
+										return (
+											<div
+												{...provided.droppableProps}
+												ref={provided.innerRef}
+												style={{
+													background: currentApp.isDraggingOver
+														? "#F8D15B"
+														: "#F5F5F5",
+													padding: 4,
+													width: 250,
+													minHeight: 500,
+												}}
+											>
+												{/* Mapping each item from list that corresponds to the column */}
+												{column.items.map((item, index) => {
+													return (
+														<Draggable
+															key={item.id}
+															draggableId={item.id}
+															index={index}
+														>
+															{(provided) => {
+																return (
+																	<div
+																		ref={provided.innerRef}
+																		{...provided.draggableProps}
+																		{...provided.dragHandleProps}
+																		style={{
+																			...provided.draggableProps.style,
+																		}}
+																	>
+																		<MentorApplicationView
+																			data={item}
+																			handleApp={updateApps}
+																			isMentor={isMentor}
+																			isNew={item.hasOwnProperty("identify")}
+																		/>
+																	</div>
+																);
+															}}
+														</Draggable>
+													);
+												})}
+												{provided.placeholder}
+											</div>
+										);
+									}}
+								</Droppable>
+							</div>
+						</div>
+					);
+				})}
+			</DragDropContext>
+		</div>
+	);
 }
 
 export default ApplicationOrganizer;

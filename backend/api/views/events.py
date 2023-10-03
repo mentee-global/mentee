e6b62d5c1b5120f62a62cbb88ad51ea3537cf85b
Solmon9099 <maximovic9099@gmail.com>
2023-10-03 02:23:01 -0400

    Fix issues for event feature

--- backend/api/views/events.py
+++ backend/api/views/events.py
@@ -127,23 +127,23 @@ def new_event(role):
             eventdate = ""
             if start_datetime_str != "":
                 eventdate = start_datetime_str + " ~ " + end_datetime_str
-            # for recipient in recipients:
-            #     res, res_msg = send_email(
-            #         recipient=recipient.email,
-            #         data={
-            #             "eventtitle": title,
-            #             "eventdate": eventdate,
-            #             "role": role_name,
-            #             recipient.preferred_language: True,
-            #             "subject": TRANSLATIONS[recipient.preferred_language][
-            #                 "new_event"
-            #             ],
-            #         },
-            #         template_id=EVENT_TEMPLATE,
-            #     )
-            #     if not res:
-            #         msg = "Failed to send new event data alert email " + res_msg
-            #         logger.error(msg)
+            for recipient in recipients:
+                res, res_msg = send_email(
+                    recipient=recipient.email,
+                    data={
+                        "eventtitle": title,
+                        "eventdate": eventdate,
+                        "role": role_name,
+                        recipient.preferred_language: True,
+                        "subject": TRANSLATIONS[recipient.preferred_language][
+                            "new_event"
+                        ],
+                    },
+                    template_id=EVENT_TEMPLATE,
+                )
+                if not res:
+                    msg = "Failed to send new event data alert email " + res_msg
+                    logger.error(msg)
         else:
             event = Event.objects.get(id=event_id)
             event.user_id = user_id

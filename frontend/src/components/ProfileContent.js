cc3524a06e7f198edcf5ec44d8b592bb9bae95d1
SolMon9099 <pmomo9099@gmail.com>
2024-11-13 15:20:09 +0800

    Fix bug in build profile page

--- frontend/src/components/ProfileContent.js
+++ frontend/src/components/ProfileContent.js
@@ -399,6 +399,15 @@ function ProfileContent(props) {
           </div>
         </>
       )}
+      {props.mentor.timezone && (
+        <>
+          <div className="mentor-profile-heading">
+            <b>{t("common.timezone")}</b>
+          </div>
+          <div>{props.mentor.timezone}</div>
+          <br />
+        </>
+      )}
 
       {props.mentor.video && (
         <div className="mentor-profile-heading">
